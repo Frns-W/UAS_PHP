1fa9cc7af7a25247a92f63aabacad332fc39fdad
Frns-W <fwhennardy@gmail.com>
2026-02-27 11:11:29 +0700

    Kategori
    Kategori barang

---
+++ kategori/proses_edit.php
@@ -0,0 +1,24 @@
+
+<?php
+    #1. Meng-koneksikan PHP ke MySQL
+    include("../koneksi.php");
+
+    #2. Mengambil Value dari Form Tambah
+    $id = $_POST['id_kategori'];
+    $kategori = $_POST['nama_kategori'];
+    
+
+    
+    #3. Query Update (proses edit data)
+    $query = "UPDATE kategori SET nama_kategori='$kategori' 
+    WHERE id_kategori='$id'";
+    
+    $tambah = mysqli_query($koneksi,$query);
+
+    #4. Jika Berhasil triggernya apa? (optional)
+    if($tambah){
+        header("location:index.php");
+    }else{
+        echo "Data Gagal diubah";
+    }
+?>
