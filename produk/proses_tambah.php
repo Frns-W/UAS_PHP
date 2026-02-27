1ba0736c891cf76cf570e4294dc6ef74392ed847
Frns-W <fwhennardy@gmail.com>
2026-02-27 11:12:04 +0700

    Produk
    proses Produk

---
+++ produk/proses_tambah.php
@@ -0,0 +1,28 @@
+<?php
+    #1. Meng-koneksikan PHP ke MySQL
+    include("../koneksi.php");
+
+    #2. Mengambil Value dari Form Tambah
+    $id_produk = $_POST['id_produk'];
+    $id_kategori = $_POST['id_kategori'];
+    $id_merk = $_POST['id_merk'];
+    $nama_produk = $_POST['nama_produk'];
+    $harga = $_POST['harga'];
+    $stok = $_POST['stok'];
+    $foto_produk = $_FILES['foto']['name'];
+    $tmp_foto = $_FILES['foto']['tmp_name'];
+
+    #3. Query Insert (proses tambah data)
+    $query = "INSERT INTO produk (id_produk,id_kategori,id_merk,nama_produk,harga,stok,foto_produk  ) 
+    VALUES ('$id_produk','$id_kategori','$id_merk','$nama_produk','$harga','$stok','$foto_produk')";
+    
+    move_uploaded_file($tmp_foto, "../fotoproduk/" . $foto_produk);
+
+    $tambah = mysqli_query($koneksi,$query);
+    #4. Jika Berhasil triggernya apa? (optional)
+    if($tambah){
+        header("location:index.php");
+    }else{
+        echo "Data Gagal ditambah";
+    }
+?>
