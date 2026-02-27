1aad181c0937d2907bcf742071bffa7e500326c4
Frns-W <fwhennardy@gmail.com>
2026-02-27 15:06:43 +0700

    proses hapus
    hapus juga dari folder

--- produk/proses_hapus.php
+++ produk/proses_hapus.php
@@ -5,14 +5,27 @@ include_once("../koneksi.php");
 #2.ID Hapus
 $dihapus = $_GET['id'];
 
-#3.menulis Query
-$query = "DELETE FROM biodata WHERE id='$dihapus'";   
+#3. Ambil nama file foto dari database
+$qry_ambil = "SELECT foto_produk FROM produk WHERE id_produk='$dihapus'";
+$hasil = mysqli_query($koneksi, $qry_ambil);
+$data = mysqli_fetch_array($hasil);
+$foto = $data['foto_produk'];
 
-#4.Menjalankan Query
+#4. Hapus file foto dari folder
+if(!empty($foto) && file_exists("../fotoproduk/".$foto)){
+    unlink("../fotoproduk/".$foto);
+}
+
+#5. menulis Query hapus data
+$query = "DELETE FROM produk WHERE id_produk='$dihapus'";   
+
+#6. Menjalankan Query
 $hapus = mysqli_query($koneksi, $query);
-unlink("../fotosiswa/".$_GET['foto']);
 
-#5.Kembali ke index.php
-header("location:index.php")
-
+#7. Kembali ke index.php
+if($hapus){
+    header("location:index.php");
+} else {
+    echo "Gagal menghapus data";
+}
 ?>
